aad12371114f63c8eb30878983fa184ff611bc73
Hugo Valla <hugo_valla@hotmail.fr>
2020-12-04 18:21:16 +0100

    searchpage down

--- src/SearchResult_JS/indexSearchResults.js
+++ src/SearchResult_JS/indexSearchResults.js
@@ -1,5 +1,9 @@
 import { searchResults } from "./searchResults";
 import { filterResult } from "./filterResults";
+import { burgerMenu } from "../burgerMenu";
+import { searchBar } from "../burgerMenu";
 
 searchResults()
 filterResult()
+burgerMenu()
+searchBar()
