f6f29377ef412d3f8ef166024d077d94ff05da69
Hugo Valla <hugo_valla@hotmail.fr>
2020-12-09 18:29:21 +0100

    untracked node_modules

--- node_modules/instantsearch.js/es/types/index.js
+++
@@ -1,6 +0,0 @@
-export * from './instantsearch';
-export * from './connector';
-export * from './widget';
-export * from './insights';
-export * from './algoliasearch';
-export * from './middleware';
