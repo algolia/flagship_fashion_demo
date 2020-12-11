018136655ee7037f895c58d9d7428a503b0491cc
Hugo Valla <hugo_valla@hotmail.fr>
2020-12-11 15:26:12 +0100

    guided navigation done

--- src/SearchResult_JS/searchResults.js
+++ src/SearchResult_JS/searchResults.js
@@ -1,6 +1,8 @@
 import instantsearch from 'instantsearch.js';
 import algoliasearch from 'algoliasearch';
 import { searchBox, clearRefinements, refinementList, stats, hits, pagination } from 'instantsearch.js/es/widgets'
+import { connectRefinementList } from 'instantsearch.js/es/connectors';
+
 
 export function searchResults() {
     const search = instantsearch({
@@ -59,8 +61,96 @@ export function searchResults() {
     ]);
 
 
+    // 1. Create a render function
+    const renderRefinementList = (renderOptions, isFirstRender) => {
+        const {
+            items,
+            isFromSearch,
+            refine,
+            createURL,
+            isShowingMore,
+            canToggleShowMore,
+            searchForItems,
+            toggleShowMore,
+            widgetParams,
+        } = renderOptions;
+
+        if (isFirstRender) {
+            // const input = document.createElement('input');
+            const ul = document.createElement('ul');
+            // const button = document.createElement('button');
+            // button.classList.add('btn-showMore')
+            // button.textContent = 'Show more';
+
+            // // input.addEventListener('input', event => {
+            // //   searchForItems(event.currentTarget.value);
+            // // });
+
+            // button.addEventListener('click', () => {
+            //     toggleShowMore();
+            // });
+
+            // widgetParams.container.appendChild(input);
+            widgetParams.container.appendChild(ul);
+            // widgetParams.container.appendChild(button);
+        }
+
+        // const input = widgetParams.container.querySelector('input');
+
+        // if (!isFromSearch && input.value) {
+        //   input.value = '';
+        // }
+
+        widgetParams.container.querySelector('ul').innerHTML = items
+            .map(
+                item => `
+          <li style="${isRefined(item)}">
+            <a
+              href="${createURL(item.value)}"
+              data-value="${item.value}"
+              style="${isRefined(item)}"
+            >
+              ${item.label} <span style="${isRefined(item)}">(${item.count})</span>
+            </a>
+          </li>
+        `
+            )
+            .join('');
+
+        [...widgetParams.container.querySelectorAll('a')].forEach(element => {
+            element.addEventListener('click', event => {
+                event.preventDefault();
+                refine(event.currentTarget.dataset.value);
+            });
+        });
+
+        // const button = widgetParams.container.querySelector('button');
+
+        // // button.disabled = !canToggleShowMore;
+        // button.textContent = isShowingMore ? 'Show less' : 'Show more';
+    };
+
+    function isRefined(item) {
+        if (item.isRefined) {
+            return 'color: white !important; background-color: rgba(0,0,0, 0.9)'
+        }
+    }
+
+    // 2. Create the custom widget
+    const customRefinementList = connectRefinementList(
+        renderRefinementList
+    );
+
+    // 3. Instantiate
+    search.addWidgets([
+        customRefinementList({
+            container: document.querySelector('#refinement-list-SearchResult'),
+            attribute: 'keywords',
+            showMoreLimit: 10,
+        })
+    ]);
 
     search.start();
 
-
 }
+
