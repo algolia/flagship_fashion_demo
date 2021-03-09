ecbee0a4ad55c4949ef2ef02cc70a00601b841ef
Hugo Valla <hugo_valla@hotmail.fr>
2021-03-09 18:11:50 +0100

    extract autocomplete logic to a single file

--- src/Homepage/app.js
+++ src/Homepage/app.js
@@ -5,6 +5,7 @@ import { burgerMenu } from "./burgerMenu";
 import { togglePersona } from "./persona";
 import { cardAnimationHome } from "./cardAnimationHome";
 import { modalProduct } from "./modalProduct";
+import { autocompleteHome } from "./autocomplete";
 
 carouselGlideJS()
 GetDataForCarousel()
@@ -12,6 +13,7 @@ carouselDetailed()
 burgerMenu()
 togglePersona()
 modalProduct()
+autocompleteHome()
 
 
 var checkExist = setInterval(function () {
