dcd2fd381f66919fff7c72b17850a436fb927905
Matthew Foyle <mfoyle93@gmail.com>
2021-03-10 13:50:18 +0000

    refactor(categorypageman, categorypagewoman, index): move demo settings into own files

--- src/Homepage/app.js
+++ src/Homepage/app.js
@@ -1,29 +1,24 @@
-import { carouselGlideJS } from "./glide";
-import { GetDataForCarousel } from "./getCarousel";
-import { carouselDetailed } from "./displayCarouselDetail";
-import { burgerMenu } from "./burgerMenu";
-import { togglePersona } from "./persona";
-import { cardAnimationHome } from "./cardAnimationHome";
-import { modalProduct } from "./modalProduct";
-import { autocompleteHome } from "./autocomplete";
+import { carouselGlideJS } from './glide';
+import { GetDataForCarousel } from './getCarousel';
+import { carouselDetailed } from './displayCarouselDetail';
+import { burgerMenu } from './burgerMenu';
+import { togglePersona } from './persona';
+import { cardAnimationHome } from './cardAnimationHome';
+import { modalProduct } from './modalProduct';
+import { autocompleteHome } from './autocomplete';
 
-carouselGlideJS()
-GetDataForCarousel()
-carouselDetailed()
-burgerMenu()
-togglePersona()
-modalProduct()
-autocompleteHome()
-
+carouselGlideJS();
+GetDataForCarousel();
+carouselDetailed();
+burgerMenu();
+togglePersona();
+modalProduct();
+autocompleteHome();
 
 var checkExist = setInterval(function () {
-    if (document.readyState === 'complete') {
-        cardAnimationHome()
-        modalProduct()
-        clearInterval(checkExist);
-    }
-}, 500)
-
-
-
-
+  if (document.readyState === 'complete') {
+    cardAnimationHome();
+    modalProduct();
+    clearInterval(checkExist);
+  }
+}, 500);
