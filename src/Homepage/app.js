aeaa7175f765cee99f723559c0b97431596752dc
yixuan WANG <yixuan.wang@algolia.com>
2021-03-31 14:33:16 +0200

    style/search: style autocomplete search bar and search layout.

--- src/Homepage/app.js
+++ src/Homepage/app.js
@@ -1,29 +1,40 @@
-import { carouselGlideJS } from "./glide";
-import { GetDataForCarousel } from "./getCarousel";
-import { carouselDetailed } from "./displayCarouselDetail";
-import { burgerMenu } from "./burgerMenu";
-import { togglePersona } from "./persona";
-import { cardAnimationHome } from "./cardAnimationHome";
-import { modalProduct } from "./modalProduct";
-import { autocompleteHome } from "./autocomplete";
+import { carouselGlideJS } from './glide';
+import { GetDataForCarousel } from './getCarousel';
+import { carouselDetailed } from './displayCarouselDetail';
+import { burgerMenu } from './burgerMenu';
+import { togglePersona } from './persona';
+import { cardAnimationHome } from './cardAnimationHome';
+import { modalProduct } from './modalProduct';
+import { autocompleteHome } from './autocomplete';
 
-carouselGlideJS()
-GetDataForCarousel()
-carouselDetailed()
-burgerMenu()
-togglePersona()
-modalProduct()
-autocompleteHome()
+carouselGlideJS();
+GetDataForCarousel();
+carouselDetailed();
+burgerMenu();
+togglePersona();
+modalProduct();
+autocompleteHome();
 
+let searchBtn = document.querySelector('#open-search');
+let searchContainer = document.querySelector('#search-row');
+let closeSearchRow = document.querySelector('#close_a');
+
+searchBtn.addEventListener('click', () => {
+  if (searchContainer) {
+    searchContainer.style.transform = 'translateY(0px)';
+  }
+});
+
+closeSearchRow.addEventListener('click', () => {
+  if (searchContainer) {
+    searchContainer.style.transform = 'translateY(-200px)';
+  }
+});
 
 var checkExist = setInterval(function () {
-    if (document.readyState === 'complete') {
-        cardAnimationHome()
-        modalProduct()
-        clearInterval(checkExist);
-    }
-}, 500)
-
-
-
-
+  if (document.readyState === 'complete') {
+    cardAnimationHome();
+    modalProduct();
+    clearInterval(checkExist);
+  }
+}, 500);
