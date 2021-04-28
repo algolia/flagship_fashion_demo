c033e9d5efc6a9a990ddc745f54f039b21512095
yixuan WANG <yixuan.wang@algolia.com>
2021-04-28 14:38:42 +0200

    change icon

--- src/Homepage/displayCarousel.js
+++ src/Homepage/displayCarousel.js
@@ -5,16 +5,23 @@ import aa from 'search-insights';
 import { createInsightsMiddleware } from 'instantsearch.js/es/middlewares';
 
 export const carousel = connectHits(function renderCarousel(
-  { bindEvent, instantSearchInstance, widgetParams: { container, title }, hits },
+  {
+    bindEvent,
+    instantSearchInstance,
+    widgetParams: { container, title },
+    hits,
+  },
   isFirstRender
 ) {
-
-  const searchClient = algoliasearch('HYDY1KWTWB', '28cf6d38411215e2eef188e635216508');
+  const searchClient = algoliasearch(
+    'HYDY1KWTWB',
+    '28cf6d38411215e2eef188e635216508'
+  );
 
   const search = instantsearch({
     indexName: 'gstar_demo_test',
     searchClient,
-    routing: true
+    routing: true,
   });
   // CONFIG TO SEND INSIGHT EVENT TO THE DASHBOARD FOR PERSONALISATION
   const insightsMiddleware = createInsightsMiddleware({
@@ -34,7 +41,7 @@ export const carousel = connectHits(function renderCarousel(
     return element;
   };
 
-  const parseInsightsEvent = element => {
+  const parseInsightsEvent = (element) => {
     const serializedPayload = element.getAttribute('data-insights-event');
 
     if (typeof serializedPayload !== 'string') {
@@ -53,23 +60,28 @@ export const carousel = connectHits(function renderCarousel(
   };
 
   if (isFirstRender) {
-    container.insertAdjacentHTML('afterbegin',
-      `<div class="title-carousel-winter"><h2>${title}</h2><a href="/searchResults.html" class="btn-carousel-winter">See All</a></div>`)
+    container.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="title-carousel-winter"><h2>${title}</h2><a href="/searchResults.html" class="btn-carousel-winter">See All</a></div>`
+    );
     const ul = document.createElement('ul');
     ul.classList.add('carousel-list-container');
     container.appendChild(ul);
 
-    container.addEventListener('click', event => {
+    container.addEventListener('click', (event) => {
       const targetWithEvent = findInsightsTarget(
         event.target,
         event.currentTarget,
-        element => element.hasAttribute('data-insights-event')
+        (element) => element.hasAttribute('data-insights-event')
       );
 
       if (targetWithEvent) {
         const payload = parseInsightsEvent(targetWithEvent);
         instantSearchInstance.sendEventToInsights(payload);
-        popUpEventClick(payload.payload.eventName, payload.payload.objectIDs[0])
+        popUpEventClick(
+          payload.payload.eventName,
+          payload.payload.objectIDs[0]
+        );
         // popUpEventCart(payload.payload.eventName, payload.payload.objectIDs[0])
       }
     });
@@ -77,34 +89,44 @@ export const carousel = connectHits(function renderCarousel(
 
   function popUpEventClick(event, object) {
     const index = searchClient.initIndex('gstar_demo_test');
-    let popUpWrapper = document.querySelector('.popUp-wrapper')
-    index.getObject(object).then(object => {
-      let div = document.createElement('div')
+    let popUpWrapper = document.querySelector('.popUp-wrapper');
+    index.getObject(object).then((object) => {
+      let div = document.createElement('div');
 
       if (event === 'Product Clicked') {
-        div.classList.add('popUpEventClick')
-        div.innerHTML = `Open product details, on ${object.name}`
+        div.classList.add('popUpEventClick');
+        div.innerHTML = `Open product details, on ${object.name}`;
       } else if (event === 'Product Added') {
-        div.classList.add('popUpEventCart')
-        div.innerHTML = `Add to cart product, on ${object.name}`
+        div.classList.add('popUpEventCart');
+        div.innerHTML = `Add to cart product, on ${object.name}`;
       }
-      popUpWrapper.appendChild(div)
+      popUpWrapper.appendChild(div);
       div.addEventListener('animationend', () => {
-        div.remove()
+        div.remove();
       });
     });
   }
 
   container.querySelector('ul').innerHTML = hits
     .map(
-      hit => `
+      (hit) => `
         <li data-id="${hit.objectID}">
-          <div class="image-wrapper" ${bindEvent('click', hit, 'Product Clicked')} data-id="${hit.objectID}">
-            <img src="${hit.image_link}" alt="${hit.name}" data-id="${hit.objectID}">
+          <div class="image-wrapper" ${bindEvent(
+            'click',
+            hit,
+            'Product Clicked'
+          )} data-id="${hit.objectID}">
+            <img src="${hit.image_link}" alt="${hit.name}" data-id="${
+        hit.objectID
+      }">
             <div class="img-overlay" data-id="${hit.objectID}"></div>
           </div>
           <div class="hit-addToCart">
-            <a ${bindEvent('click', hit, 'Product Added')}><i class="fas fa-cart-arrow-down"></i></a>
+            <a ${bindEvent(
+              'click',
+              hit,
+              'Product Added'
+            )}><i class="fas fa-ellipsis-h"></i></a>
           </div>
           <div class="info">
             <h3 class="title">${hit.name}</h3>
