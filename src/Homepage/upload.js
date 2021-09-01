083936691abb169f50c2643547963d3e46b1f14f
Alexandre Viretti <alexou73@gmail.com>
2021-09-01 18:39:46 +0200

    featt(upload): make the js

---
+++ src/Homepage/upload.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+export function toggleUpload() {
+  const btnUpload = document.querySelector('.btnUpload');
+  const upload = document.querySelector('.userUploadImage');
+  const closeUpload = document.querySelector('.closeUpload');
+  const labelUpload = document.querySelector('.labelUserUpload');
+  console.log('Event', btnUpload);
+
+  btnUpload.addEventListener('click', toggleModal);
+  closeUpload.addEventListener('click', toggleModal);
+  labelUpload.addEventListener('click', toggleModal);
+
+  function toggleModal(e) {
+    console.log('Event', upload);
+    const buttonUpload = document.querySelector('#img');
+    e.preventDefault();
+    console.log('EEEE', buttonUpload);
+    buttonUpload.onchange = function (e) {
+      console.log('Input', e);
+      let url = URL.createObjectURL(e.target.files[0]);
+
+      const files = e.target.files;
+      const name = e.target.name;
+
+      const formData = new FormData();
+      // append the files to FormData
+      Array.from(Array(files.length).keys()).map((x) => {
+        formData.append(name, files[x], files[x].name);
+      });
+      // save it
+      console.log('Form Data', formData);
+      const config = {
+        'Ocp-Apim-Subscription-Key': 'f1610ceebfb84847beee7031159a5c65',
+        'Content-Type': 'multipart/form-data',
+      };
+      axios
+        .post(
+          'https://eastus.api.cognitive.microsoft.com/vision/v3.2/detect?model-version=latest',
+          formData,
+          { headers: config }
+        )
+        .then((response) => {
+          console.log('RESP', response.data.objects[0].object);
+          let resp = response.data.objects[0].object;
+          window.location.href = `./searchResults.html?gstar_demo_test%5Bquery%5D=${resp}`;
+        })
+        .catch((err) => {
+          console.log('err', err);
+        });
+    };
+    if (upload.classList.contains('userFadeIn')) {
+      upload.classList.add('userFadeOut');
+      upload.classList.remove('userFadeIn');
+      labelUpload.style.display = 'flex';
+    } else {
+      upload.classList.add('userFadeIn');
+      upload.classList.remove('userFadeOut');
+      labelUpload.style.display = 'none';
+    }
+  }
+}
