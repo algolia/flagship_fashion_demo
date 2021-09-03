715ebd6341faf0aeec2c68891bd8dfd722254442
Alexandre Viretti <alexou73@gmail.com>
2021-09-03 15:16:10 +0200

    fix(button): try fi button upload

--- src/SearchResultPage/app.js
+++ src/SearchResultPage/app.js
@@ -2,11 +2,13 @@ import { searchResults } from './searchResults';
 import { burgerMenu } from '../Homepage/burgerMenu';
 import { cardAnimation } from './cardAnimations';
 import { togglePersona } from './persona';
+// import { toggleUpload } from '../Homepage/upload';
 import { modalProductSearchResult } from './modalProductSearchResult';
 
 searchResults();
 burgerMenu();
 togglePersona();
+// toggleUpload();
 
 var checkExist = setInterval(function () {
   if (document.readyState != 'loading') {
